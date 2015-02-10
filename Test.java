e89c6360a0f3be29f1475b2e98bfcb4aa450059a
linshi <linshimail.2011@gmail.com>
2015-02-10 12:04:02 +0800

    first local update

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test {
 	public static void main (String[] args) {
 		System.out.println("Test!!!\n");
 		System.out.println("first online change!!!\n");
+		System.out.println("first local change!!!\n");
 	}
 }
